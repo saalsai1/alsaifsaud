d0b8a82de2cc7a31ba9927d1097d7bd60f8e3f43
Saud Alsaif <Saud.alsaif5@gmail.com>
2025-04-29 00:17:51 -0700

    update 4

--- xor.py
+++ xor.py
@@ -1,11 +1,27 @@
+import random
 import sys
 
-key = sys.stdin.read().strip()
-with open(sys.argv[1], 'rb') as f:
-    data = f.read()
+key = [0] * 763
 
-key_bytes = key.encode()
-encrypted = bytes([b ^ key_bytes[i % len(key_bytes)] for i, b in enumerate(data)])
-
-with open(sys.argv[2], 'wb') as f:
-    f.write(encrypted)
+random.seed(42)
+with open(sys.argv[1], "rb") as infile:
+    while True:
+        c = sys.stdin.read(1)
+        if c:
+            for i in range(ord(c)):
+                numj = random.randint(4,250)
+                for j in range(numj):
+                    key[random.randint(0, 762)] = random.randint(1,250)
+        else:
+            break
+    outfile = open(sys.argv[2], "ab")
+    count = 0
+    while True:
+        byte = infile.read(1)
+        if byte:
+            outbyte = (int.from_bytes(byte) ^ key[count % 763] & 0x7f).to_bytes(1)
+            outfile.write(outbyte)
+            count += 1
+        else:
+            break
+#print(key)
