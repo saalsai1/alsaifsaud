9104b179b75f082fbd5db26050badd9d8b0bb96e
Saud Alsaif <Saud.alsaif5@gmail.com>
2025-04-29 01:13:02 -0700

    update

--- xor.py
+++ xor.py
@@ -4,13 +4,16 @@ def xor_crypt(data, key):
     return bytes([b ^ key[i % len(key)] for i, b in enumerate(data)])
 
 def main():
-    key = sys.stdin.read().strip()
-    key_bytes = bytes.fromhex(key)
+
+    key_hex = sys.stdin.read().strip()
+    key_bytes = bytes.fromhex(key_hex)
 
     with open(sys.argv[1], 'rb') as infile:
         data = infile.read()
 
+
     output = xor_crypt(data, key_bytes)
+
 
     with open(sys.argv[2], 'wb') as outfile:
         outfile.write(output)
